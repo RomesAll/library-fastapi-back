55b8f23c0d5ab82dc212f732c5cd31094b1831bc
RomesAll <romesky845@gmail.com>
2025-12-12 00:00:33 +0300

    Обновил модель client

--- src/api_v1/client/model.py
+++ src/api_v1/client/model.py
@@ -2,10 +2,14 @@ from config import Base
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 class ClientsORM(Base):
+    username: Mapped[str] = mapped_column(unique=True) # Попробовать другой способ с динамич. сосзданием
     first_name: Mapped[str]
     second_name: Mapped[str | None]
     address: Mapped[str]
-
+    is_active: Mapped[bool] = mapped_column(default=False)
+    phone_number: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str] # = mapped_column(hash=True)
+    
     client_books: Mapped[list["BooksORM"]] = relationship(back_populates='', secondary='reservationsorm')
 
     def __init__(self, first_name, second_name, **kw):
